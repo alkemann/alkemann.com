1a6000364798bc6cd310b34128129c0e6d7e429f
Alexander Morland <alek.morland@gmail.com>
2016-02-27 13:03:06 +0100

    Adjusted app to alkemann.com

--- webroot/index.php
+++ webroot/index.php
@@ -12,13 +12,14 @@ define('RESOURCE_DIR', 'resources');
 define('LOGS_PATH', ROOT . DS . RESOURCE_DIR . DS . 'logs' . DS);
 define('LOCALES_PATH', ROOT . DS . RESOURCE_DIR . DS . 'locales' . DS);
 define('CONFIG_PATH', ROOT . DS . RESOURCE_DIR . DS . 'configs' . DS);
-define('HL_PATH', LIBS_PATH . 'alkemann/h.l/src/alkemann/hl' . DS);
+define('HL_PATH', LIBS_PATH . 'alkemann/h.l/src' . DS );
 
 require_once(CONFIG_PATH . 'defines.php');
 require_once(LIBS_PATH . 'autoload.php');
 require_once(CONFIG_PATH . 'bootstrap.php');
 require_once(CONFIG_PATH . 'routes.php');
 
+dbp(['_config', '_classes']);
 
 try {
     $Dispatch = new alkemann\hl\core\Dispatch();
