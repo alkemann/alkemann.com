b6a37f74d115cf8d3117d9035f627704dc60a65b
Alexander Morland <alek.morland@gmail.com>
2016-03-07 19:16:21 +0100

    Started front end implementation

--- content/custom/home.html.php
+++ content/custom/home.html.php
@@ -1,6 +1,39 @@
-<h1>Home</h1>
-<p>Welcome. Try :</p>
-<ul>
-    <li><a href="/debug.html">Debug</a></li>
-    <li><a href="/sitemap.html">Sitemap</a></li>
-</ul>
+<!-- Start HTML -->
+<h1>Tasks</h1>
+<div id="task-content"></div>
+
+<!-- Templates -->
+<script id="taskListTemplate" type="text/template">
+    <form class="taskView"><input type="checkbox" class="taskDone" name="taskDone<%= id %>" value="<%= id %>" /> <span class="label"><%= title %></span>
+    <button class="remove">DEL</button></form>
+</script>
+<script id="taskEditTemplate" type="text/template">
+    <form class="taskEdit">
+        <input type="text" name="taskEdit<%= id %>" value="<%= title %>" />
+    </form>
+</script>
+
+<!-- scripts -->
+<script src="js/libs/underscore.js"></script>
+<script src="js/libs/zepto.js"></script>
+<script src="js/libs/backbone.js"></script>
+<script src="js/app.js"></script>
+<!-- App runtime -->
+<script type="text/javascript">
+    
+
+    /////////////////////////////////////////////////////////
+    // App - runtime
+    /////////////////////////////////////////////////////////
+    window.$tt = $("#task-content");
+
+    var tasks = new App.Collections.Tasks([
+      {id: 1, title: "Learn backbone"},
+      {id: 2, title: "Learn underscore"},
+      {id: 3, title: "Learn implement backend api"},
+      {id: 4, title: "Learn win"},
+    ])
+    var tasksView = new App.Views.TaskList({collection: tasks});
+    $tt.html(tasksView.render().el);
+
+</script>
---
+++ webroot/js/app.js
@@ -0,0 +1,84 @@
+(function() {
+
+///// globals
+window.App = window.App || { Models: {}, Views: {}, Collections: {}, Router: {} }
+window.t = window.t || function(id) { return _.template($("#"+id).html()); }
+
+/////////////////////////////////////////////////////////
+// Routes
+/////////////////////////////////////////////////////////
+
+
+
+/////////////////////////////////////////////////////////
+// Models
+/////////////////////////////////////////////////////////
+
+App.Models.Task = Backbone.Model.extend({
+  defaults: {id: null, title: ""}
+});
+
+
+/////////////////////////////////////////////////////////
+// Collections
+/////////////////////////////////////////////////////////
+
+App.Collections.Tasks = Backbone.Collection.extend({model: App.Models.Task});
+
+
+/////////////////////////////////////////////////////////
+// Views
+/////////////////////////////////////////////////////////
+
+// Task item view
+App.Views.TaskItem = Backbone.View.extend({
+  tagName: 'li',
+  className: 'task',
+  viewTemplate: t("taskListTemplate"),
+  editTemplate: t("taskEditTemplate"),
+  events: {
+    'dblclick .label': 'edit',
+    'submit': 'update',
+  },
+
+  // methods
+  render: function() {
+    this.$el.html( this.viewTemplate( this.model.toJSON() ) );
+    return this;
+  },
+
+  /// events
+  edit: function(e) {
+    this.$el.html( this.editTemplate( this.model.toJSON() ) );
+  },
+  update: function(e) {
+    e.preventDefault();
+    // grab new value and set to model
+    var $input = $(this.$el[0]).find("input[type=text]");
+    var newTitle = $input.val();
+    this.model.set('title', newTitle);
+    // set view back
+    this.$el.html( this.viewTemplate( this.model.toJSON() ) );
+  },
+  remove: function(e) {},
+  add: function(e) {}
+});
+
+// Task list view
+App.Views.TaskList = Backbone.View.extend({
+  tagName: 'ol',
+  style: 'color: blue',
+
+  // methods
+  render: function() {
+    this.collection.each(this.addItem, this);
+    return this;
+  },
+  addItem: function(task) {
+    var tv = new App.Views.TaskItem({model: task});
+    this.$el.append(tv.render().el);
+  }
+
+});
+
+})();
